6f951e6eeea6ca381b85408fc0a2513b28df86b9
Vladimir <v.korotkin11@gmail.com>
2018-09-30 01:49:21 +0300

    zeros: performance issue 2.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 module.exports = function getZerosCount(number) {
     let zerosAmount = 0;
-    for (let i = 5; i <= number; i+=5) {
-        let currentDividend = i;
-
-        while (currentDividend % 5 === 0) {
-              zerosAmount++;
-              currentDividend /= 5;
-        }
+    for (let i = 5; i <= number; i*=5) {
+        zerosAmount += (number - number % i) / i;
     }
     return zerosAmount;
-}
+};
 
