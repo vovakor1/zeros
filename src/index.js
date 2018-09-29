8200d811e260694ce80c2199720328ae4bfa1eaa
Vladimir <v.korotkin11@gmail.com>
2018-09-30 00:50:52 +0300

    zeros: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    const factorialNumber = number;
+    let zerosAmount = 0;
+    for (let i = factorialNumber; i > 4; i--) {
+        let currentDividend = i;
+
+        while (currentDividend % 5 === 0) {
+              zerosAmount++;
+              currentDividend = currentDividend / 5;
+        }
+    }
+    return zerosAmount;
 }
+
